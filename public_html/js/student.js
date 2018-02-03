30d34da066d4dc8753ff1ba40b0e0a8136b7f814
Daniel Cox <dannyc179@gmail.com>
2018-02-03 20:35:33 +0000

    Student Update

--- public_html/js/student.js
+++ public_html/js/student.js
@@ -4,11 +4,6 @@ $(document).ready(function () {
 });
 
 function loadStudent(studentNo) {
-    alert("SUCESS CALLING");
-    var dataToSend = {
-            studentNo: studentNo
-        }
-
     $.ajax({
             type: 'POST',
             url : 'http://localhost/public_html/php/student_info.php',
@@ -27,18 +22,30 @@ function loadStudent(studentNo) {
 }
 
 function generateTimetable(dataArray){
-    //set class to 
-    cellCount = 0;
+    var rows = 6;           //days shown
+    var columns = 10;       //times shown
+    var cellCount = 0;
+    var times = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
+    var days = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri']
+    var timetable = document.getElementById('timetable');
+    
     for (var r = 0; r < rows; r++) {
-        var tr = board.insertRow();
+        var tr = timetable.insertRow();
         for (var c = 0; c < columns; c++) {
+            var cellId = "timetable"+cellCount;
+            
             var td = tr.insertCell();
-            td.setAttribute('id', cellCount);
+            td.setAttribute('id', cellId);
+            if (cellCount<10 && cellCount>0) {
+                //add index[i] from times
+            } else if (cellCount%10 == 0) {
+                //add index[i] from days
+            }
+
+
             cellCount++;
         }
     }
-
-
 }
 
 function addData(data) {
