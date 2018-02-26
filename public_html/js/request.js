676276b18e34ca516f93a05758486c28e24d5aa1
Daniel Cox <dannyc179@gmail.com>
2018-02-26 22:56:41 +0000

    Continued work on requests

--- public_html/js/request.js
+++ public_html/js/request.js
@@ -34,15 +34,28 @@ function insertRequests(data) {
         $("#requests").append(`
         <div class="request">
              <div class="requestContent">
-                <h3>`+data[i]['title']+`</h3>
-                <p>`+data[i]['requester']+`</p>
+                <h3 class="requestTitle">`+data[i]['title']+`</h3>
+                <select class="requestAction">
+                    <option value="accept">Accept Request</option>
+                    <option value="reject">Reject Request</option>
+                    <option value="`+data[i]['requester']+`">Email Requester</option>
+                    <option value="`+data[i]['student']+`">Email Student</option>
+                </select>
+                <p title="Date and time of request creation">`+data[i]['dateTime']+`</p>
+                <hr>
+                <p title="Email address of staff requesting access.">Requester: `+data[i]['requester']+`</p>
+                <p title="Email address of student request is concerned with.">Student: `+data[i]['student']+`</p>
                 <p>`+data[i]['description']+`</p>
              </div>
              <div class="requestButtons">
-                // action dropdown menu with accept/reject etc. buttons to confirm actions
+                <!-- action dropdown menu with accept/reject etc. buttons to confirm actions -->
              </div>
         </div>
         `);
     }
+    $("#requests").append(`
+        <button type="button" id="requestConfirmButton">Click Me!</button> 
+            
+        `);
     
 }
